757ab40e8b7653348c3ec4a4e02fa27ccbac10fd
YazeedAlhanaki <tariq.almazyad@gmail.com>
2022-06-03 21:43:06 +0300

    Initial Commit

---
+++ Shared/SampleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleProjectApp.swift
+//  Shared
+//
+//  Created by Tariq Almazyad on 6/3/22.
+//
+
+import SwiftUI
+
+@main
+struct SampleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
